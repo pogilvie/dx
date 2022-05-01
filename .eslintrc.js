f5793aa5583df6d725c60b07bc86e6af5ed02385
pogilvie <code@ogilvie.us.com>
2022-05-01 06:20:17 -0700

    eslint apexrest post

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,11 @@
 module.exports = {
   extends: ["eslint-config-salesforce-typescript"],
   parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      rules: {
+        "no-console": "off"
+      },
+    }
+  ],
 };
